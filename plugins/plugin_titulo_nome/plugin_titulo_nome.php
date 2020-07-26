bfa90d358158748300409c04e7fe4f2d0d0ecd0d
pedrobarrosss <jpedro.profissional@gmail.com>
2020-07-26 20:57:32 -0300

    Criando Plugin
    Criando o plugin_titulo_nome, juntamente com o arquivo principal, contendo o código inicial.

---
+++ plugins/plugin_titulo_nome/plugin_titulo_nome.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+
+	Plugin Name: Plugin Titulo Nome
+	Description: Adiciona o nome da empresa no final do titulo de cada post
+
+*/
+
+if (!defined('ABSPATH')) {
+	die;
+}
+
+
+require_once plugin_dir_path(__FILE__) . "/plugin_titulo_nome/include/plugin_titulo_nome_adicionando_nome.php";
